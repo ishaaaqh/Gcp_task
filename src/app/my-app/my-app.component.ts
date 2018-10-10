52a7e662539f11c329051078afb082082f88425b
ishaaqh <mohammedishaandsp@gmail.com>
2018-10-10 15:36:18 +0530

    dynamic container changes

--- src/app/my-app/my-app.component.ts
+++ src/app/my-app/my-app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild,AfterViewInit } from '@angular/core';
 import Map from 'ol/Map.js';
 import View from 'ol/View.js';
+import {defaults as defaultControls, Control} from 'ol/control.js';
 import { getCenter } from 'ol/extent.js';
 import ImageLayer from 'ol/layer/Image.js';
 import Projection from 'ol/proj/Projection.js';
@@ -8,6 +9,7 @@ import Static from 'ol/source/ImageStatic.js';
 import { GCPServiceService } from '../../app/gcp-service.service';
 import { map, filter } from 'rxjs/operators';
 import { SlickModule } from 'ngx-slick';
+import * as $ from 'jquery';
 
 
 @Component({
@@ -18,7 +20,7 @@ import { SlickModule } from 'ngx-slick';
 export class MyAppComponent implements OnInit {
   public tranGcp = [];
   @ViewChild('mapElement') mapElement;
-  public currentGcp:number=1;
+  public currentGcp:number=0;
   public currentImages;                            //this is not working >>> Fix this
   constructor(private gCPServiceService: GCPServiceService) {  };
 
@@ -30,6 +32,8 @@ export class MyAppComponent implements OnInit {
    previous() {
     if(this.currentGcp>1){
      this.currentGcp--;
+     let mapRef= this.mapElement.nativeElement;
+     mapRef.innerHTML="";
      console.log('The value is '+ this.currentGcp);
      this.currentImages=this.tranGcp[this.currentGcp-1]
      console.log(this.currentImages);
@@ -40,6 +44,8 @@ export class MyAppComponent implements OnInit {
   next(){
     if(this.currentGcp<6){
      this.currentGcp++;
+     let mapRef= this.mapElement.nativeElement;
+     mapRef.innerHTML="";
      console.log('this value is ' + this.currentGcp);
      this.currentImages=this.tranGcp[this.currentGcp-1];
      this.generateMap(this.currentImages);
@@ -58,26 +64,43 @@ export class MyAppComponent implements OnInit {
     });
     console.log(data[0].image);
     console.log(this.currentImages)
+    let count = 0;
+    $('#map').siblings().empty
     for (let images of data) {
-      console.log(`https://s3aws.blob.core.windows.net/uploads/dev-site/19/{images.image}`);
-     var map = new Map({
-      layers: [
-        new ImageLayer({
-          source: new Static({
-            url: `https://s3aws.blob.core.windows.net/uploads/dev-site/19/${images.image}`,
-            projection: projection,
-            imageExtent: extent,
-          })
-        })
-      ],
-      target: mapRef,
-      view: new View({
-        projection: projection,
-        center: getCenter(extent),
-        zoom: 2,
-        maxZoom: 8,
-      })
-     });
+
+    const container = document.createElement('div');
+    $(container).insertBefore('#map');
+    $(container).css('width', '500px');
+    $(container).css('height', '500px');
+
+    $(container).attr('id', `map_${images.Easting}_${count}`);
+
+    console.log(`https://s3aws.blob.core.windows.net/uploads/dev-site/19/${images.image}`);
+    var map = new Map({
+      controls : new defaultControls({
+      attribution : false,
+      zoom : false,
+      rotate:false,
+     }),
+     layers: [
+       new ImageLayer({
+         source: new Static({
+           url: `https://s3aws.blob.core.windows.net/uploads/dev-site/19/${images.image}`,
+           projection: projection,
+           imageExtent: extent,
+         })
+       })
+     ],
+     target: `map_${images.Easting}_${count}`,
+     view: new View({
+       projection: projection,
+       center: getCenter(extent),
+       zoom: 2,
+       maxZoom: 8,
+     })
+    });
+
+     count += 1;
     }
 
       }
